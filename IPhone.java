1ed4923a2082938d3e527b09b5cfbce378379a34
Beatriz Freire <143218458+Biafreirec@users.noreply.github.com>
2023-10-21 15:32:32 -0300

    initial files

---
+++ IPhone.java
@@ -0,0 +1,11 @@
+class IPhone {
+  private Aparelho aparelho;
+  private Navegador navagador;
+  private Reprodutor reprodutor;
+
+  public IPhone(Aparelho aparelho, Navegador navagador, Reprodutor reprodutor) {
+    this.aparelho = aparelho;
+    this.navagador = navagador;
+    this.reprodutor = reprodutor;
+  }
+}
